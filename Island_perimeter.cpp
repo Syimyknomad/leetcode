07d786197e6b1d20c4de05982c306fac0e53f4b3
Syimyknomad <161936072+Syimyknomad@users.noreply.github.com>
2024-12-19 23:43:34 +0600

    Create Island_perimeter.cpp

---
+++ Island_perimeter.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        for(int i =0; i< rows;i++){
+            for(int j=0; j<cols; j++){
+                
+               
+                if (grid[i][j]==1){
+                    perimeter+=4;
+
+
+                    if(i>0 && grid[i-1][j]==1){
+                        perimeter-=1;
+                    }
+
+                    if(i<rows-1 && grid[i+1][j]==1){
+                        perimeter-=1;
+                    }
+
+                    if(j>0 && grid[i][j-1]==1){
+                        perimeter-=1;
+                    }
+
+                    if(j<cols-1 && grid[i][j+1]==1){
+                        perimeter-=1;
+
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+};
